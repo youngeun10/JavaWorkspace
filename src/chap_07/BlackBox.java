e3b55ac5f98ddc8d0f488f2ccb67c8d6b285ea21
김영은 <99572560+youngeun10@users.noreply.github.com>
2023-03-12 22:55:27 +0900

    230312_chap_07(Method)

--- src/chap_07/BlackBox.java
+++ src/chap_07/BlackBox.java
@@ -9,4 +9,26 @@ public class BlackBox {
 //     앞에 static이 붙는 변수는 클래스 변수로 BlackBox의 인스턴스에서 공통으로 사용할 수 있는 변수이다.
 //     위의 변수처럼 static이 붙지 않은 변수들은 각 인스턴스에서만 값이 유효하다.
     static boolean canAutoReport = false;       // 자동 신고 가능
+
+    void autoReport() {
+        if (canAutoReport) {
+            System.out.println("충돌이 감지되어 자동으로 신고합니다.");
+        } else {
+            System.out.println("자동 신고 기능이 지원되지 않습니다.");
+        }
+    }
+
+    void insertMemoryCard(int capacity) {
+        System.out.println("메모리 카드가 삽입되었습니다.");
+        System.out.println("용량은 " + capacity + "GB 입니다.");
+    }
+
+    int getVideoFileCount(int type) {
+        if (type == 1) {        // 일반 영상
+            return 9;
+        } else if (type == 2) { // 이벤트 영상
+            return 1;
+        }
+        return 10;
+    }
 }
